67c99b5e2a97c607b8740cb449bac7c1cabf5f14
ym247 <108989971+ym247@users.noreply.github.com>
2022-07-17 08:48:45 +0530

    bulb on off project

---
+++ indexbulb.js
@@ -0,0 +1,12 @@
+function onbulb() {
+    let bulbobj = document.querySelector("#offoff");
+    bulbobj.setAttribute("src", "/download.jpg");
+alert(Date());
+}
+function offbulb() {
+    let bulbobj = document.querySelector("#offoff");
+    bulbobj.setAttribute("src", "/bullboo.bmp");
+    alert("bulb is getting off");
+    
+
+}
